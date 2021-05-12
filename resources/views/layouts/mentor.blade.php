008f6cd849df6e9411cad94bd3498df013120569
NofathZ <nofath86@gmail.com>
2021-05-12 08:42:45 +0700

    add another resources

--- resources/views/layouts/mentor.blade.php
+++ resources/views/layouts/mentor.blade.php
@@ -12,15 +12,15 @@
   <!-- Fonts -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
   <!-- Icons -->
-  {{-- <link rel="stylesheet" href="assets/vendor/nucleo/css/nucleo.css" type="text/css"> --}}
-  {{-- <link rel="stylesheet" href="assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css"> --}}
+  {{-- <link rel="stylesheet" href="assets/libexfile/nucleo/css/nucleo.css" type="text/css"> --}}
+  {{-- <link rel="stylesheet" href="assets/libexfile/@fortawesome/fontawesome-free/css/all.min.css" type="text/css"> --}}
   <!-- Page plugins -->
   <!-- Argon CSS -->
   {{-- !! <link rel="stylesheet" href="assets/css/argon.css?v=1.2.0" type="text/css"> --}}
 
   <link href="{{ asset('css/argon.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/fortawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('vendor/nucleo/css/nucleo.css') }}" rel="stylesheet">
+  <link href="{{ asset('libexfile/fortawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('libexfile/nucleo/css/nucleo.css') }}" rel="stylesheet">
 </head>
 
 <body>
@@ -233,19 +233,19 @@
   </div>
   <!-- Argon Scripts -->
   <!-- Core -->
-  {{-- <script src="assets/vendor/jquery/dist/jquery.min.js"></script> --}}
-  <script src="{{ asset('vendor/jquery/dist/jquery.min.js') }}"></script>
-  {{-- !! <script src="assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script> --}}
-  <script src="{{ asset('vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-  {{-- <script src="assets/vendor/js-cookie/js.cookie.js"></script> --}}
-  <script src="{{ asset('vendor/js-cookie/js.cookie.js') }}"></script>
-  {{-- <script src="assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script> --}}
-  <script src="{{ asset('vendor/jquery.scrollbar/jquery.scrollbar.min.js') }}"></script>
-  {{-- <script src="assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script> --}}
-  <script src="{{ asset('vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js') }}"></script>
+  {{-- <script src="assets/libexfile/jquery/dist/jquery.min.js"></script> --}}
+  <script src="{{ asset('libexfile/jquery/dist/jquery.min.js') }}"></script>
+  {{-- !! <script src="assets/libexfile/bootstrap/dist/js/bootstrap.bundle.min.js"></script> --}}
+  <script src="{{ asset('libexfile/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+  {{-- <script src="assets/libexfile/js-cookie/js.cookie.js"></script> --}}
+  <script src="{{ asset('libexfile/js-cookie/js.cookie.js') }}"></script>
+  {{-- <script src="assets/libexfile/jquery.scrollbar/jquery.scrollbar.min.js"></script> --}}
+  <script src="{{ asset('libexfile/jquery.scrollbar/jquery.scrollbar.min.js') }}"></script>
+  {{-- <script src="assets/libexfile/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script> --}}
+  <script src="{{ asset('libexfile/jquery-scroll-lock/dist/jquery-scrollLock.min.js') }}"></script>
   <!-- Optional JS -->
-  {{-- <script src="assets/vendor/chart.js/dist/Chart.min.js"></script> --}}
-  {{-- <script src="assets/vendor/chart.js/dist/Chart.extension.js"></script> --}}
+  {{-- <script src="assets/libexfile/chart.js/dist/Chart.min.js"></script> --}}
+  {{-- <script src="assets/libexfile/chart.js/dist/Chart.extension.js"></script> --}}
   <!-- Argon JS -->
   {{-- !! <script src="assets/js/argon.js?v=1.2.0"></script> --}}
 </body>
